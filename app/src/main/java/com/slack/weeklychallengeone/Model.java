a03210118ed7ce21a2a3e2bbd6389f60b7410924
kkdroidgit <kartikdroid17@gmail.com>
2018-03-29 13:01:07 +0530

    Initial commit

---
+++ app/src/main/java/com/slack/weeklychallengeone/Model.java
@@ -0,0 +1,14 @@
+package com.slack.weeklychallengeone;
+
+public class Model {
+    private String name;
+
+    public Model(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
