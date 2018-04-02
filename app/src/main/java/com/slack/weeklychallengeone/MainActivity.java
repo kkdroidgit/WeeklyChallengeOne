0f437688abe887463db5f6ea6158ce8aede1abe3
Pavan Kuppa <kppa.pavan@gmail.com>
2018-04-02 22:45:30 +0530

    Update MainActivity.java

--- app/src/main/java/com/slack/weeklychallengeone/MainActivity.java
+++ app/src/main/java/com/slack/weeklychallengeone/MainActivity.java
@@ -1,62 +1,49 @@
 package com.slack.weeklychallengeone;
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
 import com.slack.weeklychallengeone.Utils.Model;
 import com.slack.weeklychallengeone.Utils.MyAdapter;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Model> mArrayList;
-    private MyAdapter mAdapter;
 
+    //private MyAdapter mAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        set ContentView(R.layout.ativity_main);
 
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
         initViews();
         loadData();
-
     }
-
 
     private void initViews(){
-        mRecyclerView = findViewById(R.id.recyler_view);
+        RecyclerView mRecyclerView = findViewById(R.id.recyler_view);
         mRecyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(thi);
-        mRecyclerViews.setLayoutManager(layoutManager);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
     }
 
-
-
-
-
-
-/*    private void loadData(){
-
+    private void loadData(){
         mArrayList = new ArrayList<>();
-
         mArrayList.add(new Model("Constraint Layout"));
         mArrayList.add(new Model("Linear Layout"));
         mArrayList.add(new Model("Relative Layout"));
         mArrayList.add(new Model("Card View"));
         mArrayList.add(new Model("Scroll Views"));
         mArrayList.add(new Model("Grid View"));
+        MyAdapter mAdapter = new MyAdapter(mArrayList);
+        RecyclerView mRecyclerView = findViewById(R.id.recyler_view);
+        mRecyclerView.setAdapter(mAdapter);
 
-
-
-        mAdapter = new MyAdapter(mArrayList);
-        //mRecyclerView.setAdapter(mAdapter);
-
-    }*/
-
-
+    }
 
 }
