4643473f8ab0efffe961cf91f18745f765ca5111
vinay <vinay.gupta@ssntpl.com>
2018-04-05 12:46:49 +0530

    second commit

--- app/src/main/java/com/slack/weeklychallengeone/Utils/Model.java
+++ app/src/main/java/com/slack/weeklychallengeone/Utils/Model.java
@@ -1,6 +1,7 @@
 package com.slack.weeklychallengeone.Utils;
 
 public class Model {
+
     private String name;
 
     public Model(String name) {
--- app/src/main/java/com/slack/weeklychallengeone/Utils/MyAdapter.java
+++ app/src/main/java/com/slack/weeklychallengeone/Utils/MyAdapter.java
@@ -1,6 +1,5 @@
 package com.slack.weeklychallengeone.Utils;
 
-import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,15 +19,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         this.mArrayList = mArrayList;
     }
 
-    @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout, parent, false);
         return new MyViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(MyViewHolder holder, int position) {
         holder.tv_name.setText(mArrayList.get(position).getName());
     }
 
@@ -39,11 +37,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
 
     class MyViewHolder extends RecyclerView.ViewHolder{
+
         private TextView tv_name;
+
         MyViewHolder(View view) {
             super(view);
 
-            tv_name = view.findViewById(R.id.tv_name);
+            tv_name = (TextView) view.findViewById(R.id.tv_name);
 
         }
     }
