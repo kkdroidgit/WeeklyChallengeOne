52b5883efc53a27950e69b66186034c44ca4e90c
mahima <mahima.6.ms@gmail.com>
2018-04-16 21:21:38 +0530

    weeklychallengeone - fix bug and materialize

--- app/src/main/java/com/slack/weeklychallengeone/Utils/MyAdapter.java
+++ app/src/main/java/com/slack/weeklychallengeone/Utils/MyAdapter.java
@@ -30,7 +30,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.tv_name.setText(mArrayList.get(position).getName());
+        holder.tv_desc.setText(mArrayList.get(position).getDescription());
     }
+
 
     @Override
     public int getItemCount() {
@@ -40,10 +42,12 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     class MyViewHolder extends RecyclerView.ViewHolder{
         private TextView tv_name;
+        private TextView tv_desc;
         MyViewHolder(View view) {
             super(view);
 
             tv_name = view.findViewById(R.id.tv_name);
+            tv_desc = view.findViewById(R.id.tv_desc);
 
         }
     }
