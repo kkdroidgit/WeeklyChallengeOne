7db3d0d1e18154b06b3e00f890dab3e1edbbcdba
deepakkumar <32274446+deepakkumarroy@users.noreply.github.com>
2018-05-12 16:05:40 +0530

    Weekly Challenge One By Deepak Kumar

--- app/src/main/java/com/slack/weeklychallengeone/MainActivity.java
+++ app/src/main/java/com/slack/weeklychallengeone/MainActivity.java
@@ -13,13 +13,14 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Model> mArrayList;
+    private RecyclerView mRecyclerView;
     private MyAdapter mAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        set ContentView(R.layout.ativity_main);
+        setContentView(R.layout.activity_main);
 
         initViews();
         loadData();
@@ -28,18 +29,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void initViews(){
-        mRecyclerView = findViewById(R.id.recyler_view);
+        mAdapter = new MyAdapter(mArrayList);
+        mRecyclerView = findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(thi);
-        mRecyclerViews.setLayoutManager(layoutManager);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
     }
 
 
-
-
-
-
-/*    private void loadData(){
+    private void loadData(){
 
         mArrayList = new ArrayList<>();
 
@@ -50,12 +48,10 @@ public class MainActivity extends AppCompatActivity {
         mArrayList.add(new Model("Scroll Views"));
         mArrayList.add(new Model("Grid View"));
 
-
-
         mAdapter = new MyAdapter(mArrayList);
-        //mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setAdapter(mAdapter);
 
-    }*/
+    }
 
 
 
